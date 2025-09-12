2ca9f19b6104fdd6a8062a3dd450fa20000a4bdd
Mohamed Fouad <mfb4010@gmail.com>
2025-09-12 15:42:43 +0400

    category and tag now return good thing

--- e_commerce/base/api/serializers.py
+++ e_commerce/base/api/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 # from base.models import 
 from base.models import Product
 
 class ProductSerializer(ModelSerializer):
+    categories = serializers.StringRelatedField(many=True)  # 👈 uses __str__ from Category
+    tags = serializers.StringRelatedField(many=True)   # 👈 uses __str__ from Tag
     class Meta:
         model = Product
         fields = '__all__'

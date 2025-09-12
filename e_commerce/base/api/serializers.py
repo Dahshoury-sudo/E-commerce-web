c947c9c868ba6f9b2185dd12e98932ca6b44acaa
Mohamed Fouad <mfb4010@gmail.com>
2025-09-12 16:10:46 +0400

    add reviews

--- e_commerce/base/api/serializers.py
+++ e_commerce/base/api/serializers.py
@@ -4,10 +4,11 @@ from rest_framework import serializers
 from base.models import Product
 
 class ProductSerializer(ModelSerializer):
+    average_rating = serializers.FloatField(read_only=True)  # comes from @property
     categories = serializers.StringRelatedField(many=True)  # 👈 uses __str__ from Category
     tags = serializers.StringRelatedField(many=True)   # 👈 uses __str__ from Tag
     class Meta:
         model = Product
         fields = '__all__'
-
+    
 

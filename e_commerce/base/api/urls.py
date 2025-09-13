9523639c4062a63c8c60b0a4e2521e7d368a4e52
Mohamed Fouad <mfb4010@gmail.com>
2025-09-13 20:06:45 +0400

    change the endpoints names2

--- e_commerce/base/api/urls.py
+++ e_commerce/base/api/urls.py
@@ -3,22 +3,6 @@ from . import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    #########################################################################
-    
-    path('products/',views.get_all_products),
-    path('signup/',views.register),
-    path('add-item/',views.add_item_to_cart),
-    path('remove-item-from-cart/',views.remove_item_from_cart),
-    path('edit-cart/',views.edit_cart),
-    path('add-item-to-wishlist',views.add_item_to_wishlist),
-    path('remove-item-from-wishlist',views.remove_item_from_wishlist),
-    path('recent-reviews/',views.get_recent_reviews),
-
-]
-
-urlpatterns = [
     # Auth
     path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
